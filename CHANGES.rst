dba2954b1a63905a7f3bd5e3779a32a4f8ad96c8
Michael S. Kelley <msk@hedwig>
2013-06-16 12:41:15 -0400

    New calib.py and associated datafiles.

--- CHANGES.rst
+++ CHANGES.rst
@@ -4,8 +4,8 @@
 New Features
 ^^^^^^^^^^^^
 
-- Many functions are now `astropy` `Quantity` aware.  Notably,
-  `util.Planck`.
+- Many functions are now `astropy` `Quantity` aware.  E.g.,
+  `util.Planck`, `calib.solar_flux`, `calib.cohen_standard`.
 
 Changes From mskpy v1.7.0
 ^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -25,3 +25,12 @@ Changes From mskpy v1.7.0
   - `powerlaw` renamed `randpl`.
 
   - `pcurve` renamed `polcurve`
+
+- `calib`
+
+  - `cohenstandard` renamed `cohen_standard`.
+
+  - `filtertrans` renamed `filter_trans`
+
+  - `solarflux` renamed `solar_flux`
+
--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python
+
+def find_data_files():
+    import os
+    filelist = []
+    for root, dirnames, files in os.walk('data/'):
+        dirlist = []
+        for f in files:
+            for suffix in ['.dat', '.txt', '.tbl']:
+                if f.endswith(suffix):
+                    dirlist.append(os.path.join(root, f))
+        if len(dirlist) > 0:
+            filelist.append((os.path.join('mskpy', root), dirlist))
+    return filelist
+
 if __name__ == "__main__":
     from glob import glob
     from numpy.distutils.core import setup, Extension
 
     ext1 = Extension('mskpy.lib.davint', glob('src/davint/*.f'))
+    files = find_data_files()
+    print files
 
     setup(name='mskpy',
           version='2.0.dev',
@@ -12,6 +28,7 @@ if __name__ == "__main__":
           author_email="msk@astro.umd.edu",
           url="https://github.com/mkelley/mskpy",
           packages=['mskpy', 'mskpy.lib'],
+          data_files=files,
           requires=['numpy', 'scipy', 'astropy'],
           install_requires=['numpy'],
           ext_modules=[ext1],
