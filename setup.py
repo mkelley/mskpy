d8df166c9db29eb444e4d072833667ddabbd9822
Michael S. Kelley <msk@hedwig>
2013-06-15 21:45:57 -0400

    setup update to put davint in lib.

--- setup.py
+++ setup.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python
-def configuration(parent_package='mskpy', top_path=None):
-    from numpy.distutils.misc_util import Configuration
-    from glob import glob
-    config = Configuration('', parent_package, top_path)
-    config.add_extension('davint', sources=glob('src/davint/*.f'))
-    return config
-
 if __name__ == "__main__":
-    from numpy.distutils.core import setup
+    from glob import glob
+    from numpy.distutils.core import setup, Extension
+
+    ext1 = Extension('mskpy.lib.davint', glob('src/davint/*.f'))
+
     setup(name='mskpy',
           version='2.0.dev',
           description='General purpose and astronomy related tools',
           author="Michael S. Kelley",
           author_email="msk@astro.umd.edu",
           url="https://github.com/mkelley/mskpy",
-          packages=['mskpy'],
+          packages=['mskpy', 'mskpy.lib'],
           requires=['numpy', 'scipy', 'astropy'],
           install_requires=['numpy'],
-          configuration=configuration,
+          ext_modules=[ext1],
           license='BSD',
           classifiers = [
               'Intended Audience :: Science/Research',
