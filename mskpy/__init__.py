a7de0884557790fe28461c006a10715fa560bcf3
Michael S. Kelley <msk@hedwig>
2013-07-09 09:43:03 -0400

    Update module imports.

--- mskpy/__init__.py
+++ mskpy/__init__.py
@@ -1,29 +1,35 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 """
-mskpy --- MSK's personal library for astronomy and stuff
-========================================================
+mskpy --- MSK's personal library for astronomy and stuff.
+=========================================================
 
 .. autosummary::
    :toctree: generated/
 
    Modules
    -------
-   asteroid - Defines an asteroid for observing, flux estimates.
-   calib    - Photometric calibration.
-   comet    - Defines a comet for observing, flux estimates.
-   ephem    - Solar System object ephemerides (requires PySPICE).
-   graphics - Helper functions for making plots.
-   image    - Image generators, analysis, and processing.
-   models   - Surface and dust models.
-   util     - Grab bag of utility functions.
+   asteroid    - Defines an asteroid for observing, flux estimates.
+   calib       - Photometric calibration.
+   comet       - Defines a comet for observing, flux estimates.
+   ephem       - Solar System object ephemerides (requires PySPICE).
+   graphics    - Helper functions for making plots.
+   image       - Image generators, analysis, and processing.
+   instruments - 
+   models      - Surface and dust models.
+   util        - Grab bag of utility functions.
 
 """
 
-from . import calib
 from . import image
-from . import models
 from . import util
+
+from .util import *
+from .image import *
+
+from . import calib
+from . import instruments
+from . import models
 
 # the following block depends on PySPICE
 try:
@@ -37,11 +43,13 @@ if spice is not None:
     from . import comet
 
     from .ephem import *
-    from comet import *
-    from asteroid import *
+    from .comet import *
+    from .asteroid import *
+# end dependency on PySPICE
 
 # depends on matplotlib
 try:
     from . import graphics
 except ImportError:
     pass
+# end matplotlib
