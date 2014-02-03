5aebbe21834fee7cc25b239c7f9a4eec041d5b03
Michael S. Kelley <msk@astro.umd.edu>
2014-02-03 11:41:21 -0500

    catalogs: fix triangle shape error; refactor match_cat output.

--- mskpy/catalogs.py
+++ mskpy/catalogs.py
@@ -19,7 +19,7 @@ __all__ = [
 
 import numpy as np
 
-def match_cat(cat0, cat1, tol=0.01, verbose=True):
+def match_cat(cat0, cat1, tol=0.01, full_output=False, verbose=True):
     """Find matching stars between two catalogs.
 
     Parameters
@@ -28,23 +28,29 @@ def match_cat(cat0, cat1, tol=0.01, verbose=True):
       Each catalog is an 2xN array of (y, x) positions.
     tol : float, optional
       The match tolerance.
+    full_output : bool, optional
+      Set to `True` to also return `match_matrix`.
     verbose : bool, optional
       Print some feedback for the user.
 
     Returns
     -------
-    matches : ndarray
-      The best match for star `i` of `cat0` is index `matches[i]` in
-      `cat1`.
-    confidence : ndarray
-      The number of times star `i` of `cat0` was matched to
-      `matches[i]` of `cat1`.
+    matches : dictionary
+      The best match for star `i` of `cat0` is `matches[i]` in `cat1`.
+      Stars that are matched multiple times, or not matched at all,
+      are not returned.
+    score : dictionary
+      Fraction of times star `i` matched star `matches[i]` out of all
+      times stars `i` and `matches[i]` were matched to any star.
+    match_matrix : ndarray, optional
+      If `full_output` is `True`, also return the matrix of all star
+      matches.
 
     Notes
     -----
 
     Based on the description of DAOPHOT's catalog matching via
-    triagles at
+    triangles at
     http://ned.ipac.caltech.edu/level5/Stetson/Stetson5_2.html
 
     """
@@ -53,25 +59,39 @@ def match_cat(cat0, cat1, tol=0.01, verbose=True):
 
     v0, s0 = triangles(*cat0)
     v1, s1 = triangles(*cat1)
+    N0 = len(cat0[0])
+    N1 = len(cat1[0])
     tree = cKDTree(s0)
     d, i = tree.query(s1)  # nearest matches between triangles
 
     if verbose:
         print ("""[match_cat] cat0 = {} triangles, cat1 = {} triangles
-[match_cat] Best match score = {}, worst match sorce = {}
-[match_cat] {} matches at or below given tolerance ({})""".format(
+[match_cat] Best match score = {:.2g}, worst match sorce = {:.2g}
+[match_cat] {} triangle pairs at or below given tolerance ({})""".format(
                 len(v0), len(v1), min(d), max(d), sum(d <= tol), tol))
-    
-    matched = np.zeros((len(cat0[0]), len(cat1[0])), int)
+
+    match_matrix = np.zeros((N0, N1), int)
     for k, j in enumerate(i):
         if d[k] <= tol:
-            matched[v0[j][0], v1[k][0]] += 1
-            matched[v0[j][1], v1[k][1]] += 1
-            matched[v0[j][2], v1[k][2]] += 1
+            match_matrix[v0[j][0], v1[k][0]] += 1
+            match_matrix[v0[j][1], v1[k][1]] += 1
+            match_matrix[v0[j][2], v1[k][2]] += 1
 
-    matches = np.argmax(matched, 1)
-    confidence = np.max(matched, 1)
-    return matches, confidence
+    m0 = match_matrix.argmax(1)
+    m1 = match_matrix.argmax(0)
+    matches = dict()
+    scores = dict()
+    for i in range(len(m0)):
+        if i == m1[m0[i]]:
+            matches[i] = m0[i]
+            peak = match_matrix[i, m0[i]] * 2
+            total = match_matrix[i, :].sum() + match_matrix[:, m0[i]].sum()
+            scores[i] = peak / float(total)
+
+    if full_output:
+        return matches, scores, match_matrix
+    else:
+        return matches, scores
 
 def triangles(y, x):
     """Describe all possible triangles in a set of points.
@@ -94,8 +114,8 @@ def triangles(y, x):
     from itertools import combinations
 
     v = np.array(list(combinations(range(len(y)), 3)))
-    dy = y[v] - np.roll(y[v], 1)
-    dx = x[v] - np.roll(x[v], 1)
+    dy = y[v] - np.roll(y[v], 1, 1)
+    dx = x[v] - np.roll(x[v], 1, 1)
     abc = np.sort(np.sqrt(dy**2 + dx**2), 1)[:, ::-1]  #  lengths of sides abc
     shapes = abc[:, 1:] / abc[:, :1]  # b/a, c/a
 
