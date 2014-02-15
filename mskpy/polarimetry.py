bc9e576ab49a930eecea1f76269e150992a0ae55
MSK <davenportman@gmail.com>
2014-02-15 13:39:13 -0500

    Work on polarimetry.
    Combine linear_pol and linear_pol_angle.
    
    Add p_eff.
    
    Add LinPol.table method.

--- mskpy/polarimetry.py
+++ mskpy/polarimetry.py
@@ -12,7 +12,6 @@ polarimetry --- Polarization tools.
    Functions
    ---------
    linear_pol
-   linear_pol_angle
    ricean_correction
 
 """
@@ -37,9 +36,12 @@ class LinPol(object):
       `Q/I`, `U/I`.
     sig_I, sig_QI, sig_UI : float or array, optional
       `I`, `QI` and `UI` uncertainties.
-    correct : bool, optional
-      Set to `False` to prevent the Ricean correction from being
-      applied.
+    p_eff : float, optional
+      Polarization efficiency correction.
+    rc_correct : bool, optional
+      If `True`, enable the Ricean correction.
+    eff_correct : bool, optional
+      If `True`, enable the polarization efficiency correction.
 
     Attributes
     ----------
@@ -54,12 +56,13 @@ class LinPol(object):
 
     Methods
     -------
-    rotate - This linear polarization vector, rotated.
+    rotate - This linear polarization object, rotated.
+    table - A table of results.
 
     """
 
     def __init__(self, I, QI, UI, sig_I=None, sig_QI=None, sig_UI=None,
-                 correct=True):
+                 p_eff=1.0, sig_p_eff=0.0, rc_correct=True, p_eff_correct=True):
         self.I = I
         self.QI = QI
         self.UI = UI
@@ -68,21 +71,27 @@ class LinPol(object):
         self.sig_QI = sig_QI
         self.sig_UI = sig_UI
 
-        self.correct = correct
+        self.p_eff = p_eff
+        self.sig_p_eff = sig_p_eff
+
+        self.p_eff_correct = p_eff_correct
+        self.rc_correct = rc_correct
 
     @property
     def _pth(self):
-        from astropy.coordinates import Angle
-        args = self.QI, self.UI, self.sig_QI, self.sig_UI
-        p = linear_pol(*args)
-        th = linear_pol_angle(*args)
-        th = (Angle(th[0] * u.deg),
-              None if th[1] is None else Angle(th[1] * u.deg))
-        if self.correct:
-            return ricean_correction(*(p + th))
+        if self.p_eff_correct:
+            kwargs = dict(p_eff=self.p_eff, sig_p_eff=self.sig_p_eff)
         else:
-            return p + th
+            kwargs = dict(p_eff=1.0, sig_p_eff=0.0)
 
+        pth = linear_pol(self.QI, self.UI, self.sig_QI, self.sig_UI,
+                         **kwargs)
+
+        if self.rc_correct:
+            pth = ricean_correction(*pth)
+
+        return pth
+            
     @property
     def p(self):
         return self._pth[0]
@@ -104,9 +113,9 @@ class LinPol(object):
 
         The vector is rotated by the amount `a`, e.g.::
           >>> print pol.theta
-          <Angle 0 deg>
+          0.0
           >>> print pol.rotate(90 * u.deg).theta
-          <Angle 90 deg>
+          90.0
 
         Parameters
         ----------
@@ -124,8 +133,8 @@ class LinPol(object):
         s = np.sin(2 * a)
 
         # We want uncorrected vectors
-        correct = self.correct
-        self.correct = False
+        correct = self.rc_correct
+        self.rc_correct = False
 
         QI =  self.QI * c + self.UI * s
         UI = -self.QI * s + self.UI * c
@@ -136,9 +145,42 @@ class LinPol(object):
             sig_QI =  self.sig_QI * c + self.sig_UI * s
             sig_UI = -self.sig_QI * s + self.sig_UI * c
 
-        self.correct = correct
+        self.rc_correct = correct
         return LinPol(I, QI, UI, sig_I=sig_I, sig_QI=sig_QI, sig_UI=sig_UI,
                       correct=correct)
+
+    def table(self):
+        """The data, formatted as a table.
+
+        Best for 1D vectors of intensity/polarization.
+
+        Returns
+        -------
+        tab : astropy.table.Table
+          A table of I, Q, U, P, and th (and, potentially,
+          uncertainties).
+
+        """
+        from astropy.table import Table
+        data = (self.I, self.sig_I,
+                self.QI, self.sig_QI,
+                self.UI, self.sig_UI,
+                self.p, self.sig_p,
+                self.theta, self.sig_theta)
+        names = ['I', 'sig_I',
+                 'QI', 'sig_QI',
+                 'UI', 'sig_UI',
+                 'P', 'sig_P',
+                 'th', 'sig_th']
+        if self.sig_I is None:
+            tab = Table(data[::2], names=names[::2])
+        else:
+            tab = Table(data, names=names)
+        tab.meta['rc_applied'] = self.rc_correct
+        tab.meta['p_eff_applied'] = self.p_eff_correct
+        tab.meta['p_eff'] = self.p_eff
+        tab.meta['sig_p_eff'] = self.sig_p_eff
+        return tab
 
 class HalfWavePlate(LinPol):
     """Linear polarimetry with a 1/2-wave plate.
@@ -150,9 +192,11 @@ class HalfWavePlate(LinPol):
       I135]`.  Each intensity may in turn be an array.
     sig_I : array, optional
       Intensity uncertainties, same form as `I`.
-    correct : bool, optional
-      Set to `False` to prevent the Ricean correction from being
-      applied.
+    p_eff, sig_p_eff : float, optional
+      Polarization efficiency and uncertainty.
+    rc_correct : bool, optional
+    eff_correct : bool, optional
+      Ricean and polarization efficiency correction flags.  See `LinPol`.
     flipU : bool, optional
       Set to `True` to reverse the sense of the `U` parameter.
 
@@ -176,7 +220,8 @@ class HalfWavePlate(LinPol):
 
     """
 
-    def __init__(self, I, sig_I=None, correct=True, flipU=False):
+    def __init__(self, I, sig_I=None, p_eff=1.0, sig_p_eff=0.0,
+                 rc_correct=True, p_eff_correct=True, flipU=False):
         self.I0 = I[0]
         self.I45 = I[1]
         self.I90 = I[2]
@@ -193,7 +238,11 @@ class HalfWavePlate(LinPol):
             self.sig_I90 = sig_I[2]
             self.sig_I135 = sig_I[3]
 
-        self.correct = correct
+        self.p_eff = p_eff
+        self.sig_p_eff = sig_p_eff
+            
+        self.rc_correct = rc_correct
+        self.p_eff_correct = p_eff_correct
         self.flipU = flipU
 
     def __getitem__(self, k):
@@ -248,8 +297,8 @@ class HalfWavePlate(LinPol):
             return (np.sqrt(self.sig_I45**2 + self.sig_I135**2)
                     / (self.I45 + self.I135))
 
-def linear_pol(QI, UI, sig_QI=None, sig_UI=None):
-    """Compute total linear polarization.
+def linear_pol(QI, UI, sig_QI=None, sig_UI=None, p_eff=1.0, sig_p_eff=0.0):
+    """Compute total linear polarization and angle.
 
     The Ricean correction is not applied.
 
@@ -259,49 +308,39 @@ def linear_pol(QI, UI, sig_QI=None, sig_UI=None):
       Stokes parameters Q/I and U/I.
     sig_QI, sig_UI : float or ndarray, optional
       Uncertainties in Q/I and U/I.
+    p_eff : float, optional
+      The polarization efficiency.
+    sig_p_eff : float, optional
+      The polarization efficiency uncertainty.
 
     Returns
     -------
     p, sig_p : float or ndarray
       Total linear polarization and uncertainty.  `sig_p` will be None
       if either of `sig_QI` or `sig_UI` is `None`.
-
-    """
-
-    p = np.sqrt(QI**2 + UI**2)
-    if any([sig_QI is None, sig_UI is None]):
-        sig_p = None
-    else:
-        sig_p = np.sqrt((sig_QI * QI)**2 + (sig_UI * UI)**2) / p
-    return p, sig_p
-
-def linear_pol_angle(QI, UI, sig_QI, sig_UI):
-    """Compute linear polarization position angle.
-
-    Parameters
-    ----------
-    QI, UI : float or ndarray
-      Stokes parameters Q/I and U/I.
-    sig_QI, sig_UI : float or ndarray, optional
-      Uncertainties in Q/I and U/I.
-
-    Returns
-    -------
     theta, sig_theta : float or ndarray
       Position angle and uncertainty.  `sig_theta` will be `None` if
       either of `sig_QI` or `sig_UI` is `None`. [degrees]
 
     """
 
-    p, sig_p = linear_pol(QI, UI, sig_QI, sig_UI)
+    p = np.sqrt(QI**2 + UI**2) / p_eff
     theta = np.degrees(np.arctan2(UI, QI) / 2.0)
     theta = (theta + 720) % 180.0
-    if sig_p is None:
+
+    if any([sig_QI is None, sig_UI is None]):
+        sig_p = None
         sig_theta = None
     else:
+        #sig_p = np.sqrt((sig_QI * QI)**2 + (sig_UI * UI)**2) / p
+        #sig_QIcor2 = (sig_QI**2 + UI**2 * sig_peff**2) / peff**2
+        #sig_UIcor2 = (sig_UI**2 + QI**2 * sig_peff**2) / peff**2
+        sig_p = np.sqrt((sig_QI * QI)**2 + (sig_UI * UI)**2
+                        + 2 * QI**2 * UI**2 * sig_p_eff**2) / p_eff / p
         sig_theta = np.maximum(0, np.degrees(sig_p / p) / 2.0)
         sig_theta = np.minimum(sig_theta, 180)
-    return theta, sig_theta
+
+    return p, sig_p, theta, sig_theta
 
 def ricean_correction(p, sig_p, theta, sig_theta):
     """Apply Ricean correction to the polarization and angle.
