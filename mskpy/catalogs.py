7192d8174406d97d48ca14739d10956b70f23343
Michael S. P. Kelley <msk@astro.umd.edu>
2015-04-13 17:19:01 -0400

    Work on catalogs.spatial_match. Add brightness.

--- mskpy/catalogs.py
+++ mskpy/catalogs.py
@@ -7,12 +7,16 @@ catalogs - Tools for working with lists of stars
 .. autosummary::
    :toctree: generated/
 
+   brightest
+   project_catalog
    spatial_match
    triangles
 
 """
 
 __all__ = [
+    'brightest',
+    'project_catalog',
     'spatial_match',
     'triangles'
 ]
@@ -61,23 +65,71 @@ def project_catalog(cat, wcs=None):
     x, y = cat.to_pixel(wcs)
     return np.vstack((y, x))
 
-def spatial_match(cat0, cat1, n=30, tol=0.01, min_score=0, full_output=False,
-                  verbose=True, **kwargs):
+def brightest(cat0, flux, n):
+    """Return the n brightest objects in the catalog.
+
+    Parameters
+    ----------
+    cat0 : array
+      2xN array of positions.
+    flux : array
+      N-element array of object brightness.
+    n : int
+      Return the brightest `n` objects.
+
+    Returns
+    -------
+    cat : array
+
+    """
+    return cat0[:, np.argsort(flux)[::-1][:n]]
+
+def spatial_match(cat0, cat1, tol=0.01, a2c_tol=1.0, cbet_tol=0.2,
+                  psig=1.5, min_frac=0, full_output=False, verbose=True,
+                  **kwargs):
 
     """Find spatially matching sources between two lists.
+
+    Following IRAF's ccxymatch, compare the shapes and orientations of
+    triangles within two catalog lists:
+
+      1) For each triangle, find the nearest neighbor in `(a/c,
+      cos(beta))` space, where `a` and `c` are the longest and shorest
+      sides, respectively, and `beta` is the angle between the
+      longtest and shortest side (i.e., the opening angle of the
+      vertex opposite side `b`).
+
+      2) If the distance between two triangles in `(a/c, cos(beta))`
+      space is `d`, then ignore triangles with `d < tol`.
+
+      3) Compute the ratio of the matched triangle perimeters, and
+      reject poor matches based on a sigma-clipping algorithm.  This
+      step essentially finds the scale factor between the two
+      catalogs.
+
+      4) Compare matched triangle orientations, and compute the number
+      that match in the clockwise sense.  If this is more than half,
+      then reject those that match in the counter-clockwise sense,
+      otherwise, reject the clockwise matches.
+
+      5) Finally, for each triangle find the actual stars that match.
+      Optionally, remove stars that do not match a given fraction of
+      the time.
 
     Parameters
     ----------
     cat0, cat1 : arrays
-      Each catalog is a 2xN array of (y, x) positions, or a 3xN array
-      of positions and flux (y, x, f).
-    n : float, optional
-      If fluxes are provided in the catalogs, only the brightest `n`
-      stars are considered.
+      Each catalog is a 2xN array of (y, x) positions.
     tol : float, optional
-      The match tolerance.
-    min_score : float, optional
-      Only return matches with scores greater than `min_score`.
+      The match tolerance in (a/c, cos(beta)) space.
+    a2c_tol : float, optional
+      A tolerance factor for the a/c ratio matching.
+    cbet_tol : float, optional
+      A tolerance factor for the cos(beta) matching.
+    psig : float, optional
+      Clip perimeter ratios at this sigma.
+    min_frac : float, optional
+      Only return stars with `frac > min_frac`.
     full_output : bool, optional
       Set to `True` to also return `match_matrix`.
     verbose : bool, optional
@@ -91,48 +143,80 @@ def spatial_match(cat0, cat1, n=30, tol=0.01, min_score=0, full_output=False,
       The best match for star `i` of `cat0` is `matches[i]` in `cat1`.
       Stars that are matched multiple times, not matched at all, or
       with scores less than `min_score` are not returned.
-    score : dictionary
+    frac : dictionary
       Fraction of times star `i` matched star `matches[i]` out of all
       times stars `i` and `matches[i]` were matched to any star.
     match_matrix : ndarray, optional
       If `full_output` is `True`, also return the matrix of all star
       matches.
+    scores : ndarray, optional
+      The scores for each triangle's nearest neighbor.
 
     Notes
     -----
 
-    Based on the description of DAOPHOT's catalog matching via
-    triangles at
-    http://ned.ipac.caltech.edu/level5/Stetson/Stetson5_2.html
+    I don't think step 5 is robust.
 
     """
 
     from scipy.spatial.ckdtree import cKDTree
+    from .util import takefrom, meanclip
 
-    if len(cat0) == 3:
-        cat0 = cat0[:, np.argsort(cat0[2])[::-1][:n]][:2]
+    print "\n*** spatial_match is experimental! ***\n"
+
     assert len(cat0) == 2
     v0, s0 = triangles(*cat0, **kwargs)
 
-    if len(cat1) == 3:
-        cat1 = cat1[:, np.argsort(cat1[2])[::-1][:n]][:2]
     assert len(cat1) == 2
     v1, s1 = triangles(*cat1, **kwargs)
 
     N0 = len(cat0[0])
     N1 = len(cat1[0])
-    tree = cKDTree(s0)
-    d, i = tree.query(s1)  # nearest matches between triangles
+
+    # Find closest triangles, considering a/c and cos(beta)
+    scale = 1.0 / np.array((a2c_tol, cbet_tol))
+    tree = cKDTree(s0[:, 1:3] * scale)
+    d, i = tree.query(s1[:, 1:3] * scale)
+
+    # reject based on tolerance
+    good = d <= tol
 
     if verbose:
         print ("""[spatial_match] cat0 = {} triangles, cat1 = {} triangles
 [spatial_match] Best match score = {:.2g}, worst match sorce = {:.2g}
-[spatial_match] {} triangle pairs at or below given tolerance ({})""".format(
-                len(v0), len(v1), min(d), max(d), sum(d <= tol), tol))
+[spatial_match] {} triangle pairs at or below given tolerance ({})""").format(
+                len(v0), len(v1), min(d), max(d), sum(d <= tol), tol)
+
+    # reject based on perimeter
+    perimeter_ratios = s1[:, 0] / s0[i, 0]
+    mc = meanclip(perimeter_ratios[good], lsig=psig, hsig=psig,
+                  full_output=True)
+    p_good = (np.abs(perimeter_ratios - mc[0]) / mc[1] <= psig) * good
+    good *= p_good
+
+    if verbose:
+        print ("""[spatial_match] Sigma-clipped perimeter ratios = {} +/- {}
+[spatial_match] {} triangle pairs with perimeter ratios within {} sigma.""").format(
+    mc[0], mc[1], p_good.sum(), psig)
+
+    # reject based on orientation of vertices
+    ccw = s0[i, 3] == s1[:, 3]
+    cw_count = (~ccw[good]).sum()
+    ccw_count = ccw[good].sum()
+    if ccw_count >= cw_count:
+        good *= ccw
+    else:
+        good *= ~ccw
+
+    if verbose:
+        print ("""[spatial_match] Orientation matches = {}{}
+[spatial_match] Anti-orientation matches = {}{}""").format(
+    cw_count, " (rejected)" if cw_count <= ccw_count else "",
+    ccw_count, " (rejected)" if cw_count > ccw_count else "")
 
     match_matrix = np.zeros((N0, N1), int)
     for k, j in enumerate(i):
-        if d[k] <= tol:
+        if good[k]:
             match_matrix[v0[j][0], v1[k][0]] += 1
             match_matrix[v0[j][1], v1[k][1]] += 1
             match_matrix[v0[j][2], v1[k][2]] += 1
@@ -140,22 +224,22 @@ def spatial_match(cat0, cat1, n=30, tol=0.01, min_score=0, full_output=False,
     m0 = match_matrix.argmax(1)
     m1 = match_matrix.argmax(0)
     matches = dict()
-    scores = dict()
+    frac = dict()
     for i in range(len(m0)):
         if i == m1[m0[i]]:
             matches[i] = m0[i]
             peak = match_matrix[i, m0[i]] * 2
             total = match_matrix[i, :].sum() + match_matrix[:, m0[i]].sum()
-            scores[i] = peak / float(total)
+            frac[i] = peak / float(total)
 
     for k in matches.keys():
-        if scores[k] < min_score:
-            del matches[k], scores[k]
+        if frac[k] < min_frac:
+            del matches[k], frac[k]
 
     if full_output:
-        return matches, scores, match_matrix
+        return matches, frac, match_matrix, d
     else:
-        return matches, scores
+        return matches, frac
 
 def triangles(y, x, max_ratio=10, min_sep=0):
     """Describe all possible triangles in a set of points.
@@ -184,7 +268,7 @@ def triangles(y, x, max_ratio=10, min_sep=0):
       each triangle (Nx3).
     s : array
       The shape of each triangle (Nx4): perimeter, a/c, cos(beta),
-      curl.
+      orientation.
 
     """
 
