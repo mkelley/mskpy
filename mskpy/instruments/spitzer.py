46db6df2d18dc33ced67d64c33edbcbcfd8c28ee
Michael S. P. Kelley <msk@astro.umd.edu>
2016-01-28 14:39:31 -0500

    Add ability to only compute some of the 4 filters in IRAC.ccorrection()
    Also, comment out the not yet finished IRAC.ccorrection_tab

--- mskpy/instruments/spitzer.py
+++ mskpy/instruments/spitzer.py
@@ -40,7 +40,7 @@ class IRAC(Camera):
         location = Spitzer
         Camera.__init__(self, w, shape, ps, location=location)
 
-    def ccorrection(self, sf):
+    def ccorrection(self, sf, channels=[1, 2, 3, 4]):
         """IRAC color correction.
 
         Seems to agree within 1% of the IRAC Instrument Handbook.
@@ -51,6 +51,9 @@ class IRAC(Camera):
         sf : function
           A function that generates source flux density as a Quantity
           given wavelength as a Quantity.
+        channels : list, optional
+          A list of the IRAC channels for which to compute the color
+          correction, e.g., `[1, 2]` for 3.6 and 4.5 um.
 
         Returns
         -------
@@ -65,13 +68,14 @@ class IRAC(Camera):
         from ..util import davint, takefrom
 
         nu0 = (const.c.si / self.wave).to(u.teraHertz).value
-        K = np.zeros(4)
-        for i in range(4):
-            tw, tr = filter_trans('IRAC CH{:}'.format(i + 1))
+        K = np.zeros(len(channels))
+        for ch in channels:
+            tw, tr = filter_trans('IRAC CH{:}'.format(ch))
             nu = (const.c / tw).to(u.teraHertz).value
 
             sfnu = sf(tw).to(u.Jy, u.spectral_density(tw)).value
 
+            i = ch - 1  # self.wave index
             sfnu /= sf(self.wave[i]).to(u.Jy, u.spectral_density(self.wave[i])).value
 
             sfnu, tr, nu = takefrom((sfnu, tr, nu), nu.argsort())
@@ -80,48 +84,48 @@ class IRAC(Camera):
 
         return K
 
-    def ccorrection_tab(self, sw, sf):
-        """IRAC color correction of a tabulated spectrum.
-
-        Parameters
-        ----------
-        sw : Quantity
-          Source wavelength.
-        sf : Quantity
-          Source flux density.
-
-        Returns
-        -------
-        K : ndarray
-          Color correction: `Fcc = F / K`.
-
-        """
-
-        from scipy import interpolate
-        import astropy.constants as const
-        from ..calib import filter_trans
-        from ..util import davint, takefrom
-
-        nu0 = (const.c.si / self.wave).to(u.teraHertz).value
-        K = np.zeros(4)
-        for i in range(4):
-            tw, tr = filter_trans('IRAC CH{:}'.format(i + 1))
-            nu = (const.c / tw).to(u.teraHertz).value
-
-            # interpolate the filter transmission to a higher
-            # resolution
-            t
-
-            s = interpolate.splrep(sw.value, sf.value)
-            _sf = interpolate.splev(fw.value, s, ext=1)
-            _sf /= interpolate.splev(self.wave[i].value, s, ext=1)
-
-            _sf *= sf.unit.to(u.Jy, u.spectral_density(fw))
-
-            _sf, ft, nu = takefrom((_sf, ft, nu), nu.argsort())
-            K[i] = (davint(nu, _sf * ft * nu0[i] / nu, nu[0], nu[-1])
-                    / davint(nu, ft * (nu0[i] / nu)**2, nu[0], nu[-1]))
-        return K
+#    def ccorrection_tab(self, sw, sf):
+#        """IRAC color correction of a tabulated spectrum.
+#
+#        Parameters
+#        ----------
+#        sw : Quantity
+#          Source wavelength.
+#        sf : Quantity
+#          Source flux density.
+#
+#        Returns
+#        -------
+#        K : ndarray
+#          Color correction: `Fcc = F / K`.
+#
+#        """
+#
+#        from scipy import interpolate
+#        import astropy.constants as const
+#        from ..calib import filter_trans
+#        from ..util import davint, takefrom
+#
+#        nu0 = (const.c.si / self.wave).to(u.teraHertz).value
+#        K = np.zeros(4)
+#        for i in range(4):
+#            tw, tr = filter_trans('IRAC CH{:}'.format(i + 1))
+#            nu = (const.c / tw).to(u.teraHertz).value
+#
+#            # interpolate the filter transmission to a higher
+#            # resolution
+#            t
+#
+#            s = interpolate.splrep(sw.value, sf.value)
+#            _sf = interpolate.splev(fw.value, s, ext=1)
+#            _sf /= interpolate.splev(self.wave[i].value, s, ext=1)
+#
+#            _sf *= sf.unit.to(u.Jy, u.spectral_density(fw))
+#
+#            _sf, ft, nu = takefrom((_sf, ft, nu), nu.argsort())
+#            K[i] = (davint(nu, _sf * ft * nu0[i] / nu, nu[0], nu[-1])
+#                    / davint(nu, ft * (nu0[i] / nu)**2, nu[0], nu[-1]))
+#        return K
 
 # update module docstring
 from ..util import autodoc
