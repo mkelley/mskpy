02153e80fa234c2863837b0ac9151962caa9fc4d
Michael S. P. Kelley <msk@astro.umd.edu>
2018-05-07 15:59:22 -0400

    Fix crash when no continuum color passed to flux_oh

--- mskpy/photometry/hb.py
+++ mskpy/photometry/hb.py
@@ -53,7 +53,7 @@ filters = set(['OH', 'NH', 'CN', 'C3', 'CO+', 'C2', 'H2O+', 'UC',
 all_filters = filters | set(['R', 'V', 'SDSS-R', 'SDSSr'])
 
 cw = {  # center wavelengths
-      'OH': u.Quantity(0.3097, 'um'),
+    'OH': u.Quantity(0.3097, 'um'),
       'NH': u.Quantity(0.3361, 'um'),
       'UC': u.Quantity(0.3449, 'um'),
       'CN': u.Quantity(0.3869, 'um'),
@@ -71,40 +71,40 @@ cw = {  # center wavelengths
 }
 
 cw_50 = {  # 50% power width
-      'OH': u.Quantity( 58, 'AA'),
-      'NH': u.Quantity( 54, 'AA'),
-      'UC': u.Quantity( 79, 'AA'),
-      'CN': u.Quantity( 56, 'AA'),
-      'C3': u.Quantity( 58, 'AA'),
-     'CO+': u.Quantity( 64, 'AA'),
-      'BC': u.Quantity( 61, 'AA'),
+    'OH': u.Quantity(58, 'AA'),
+      'NH': u.Quantity(54, 'AA'),
+      'UC': u.Quantity(79, 'AA'),
+      'CN': u.Quantity(56, 'AA'),
+      'C3': u.Quantity(58, 'AA'),
+     'CO+': u.Quantity(64, 'AA'),
+      'BC': u.Quantity(61, 'AA'),
       'C2': u.Quantity(119, 'AA'),
-      'GC': u.Quantity( 56, 'AA'),
+      'GC': u.Quantity(56, 'AA'),
     'H2O+': u.Quantity(164, 'AA'),
-      'RC': u.Quantity( 58, 'AA')
+      'RC': u.Quantity(58, 'AA')
 }
 
 # Table VI of Farnham et al. 2000
 F_0 = {  # Zero magnitude flux density
-      'OH': u.Quantity(10.560e-8, 'W/(m2 um)'),
-      'NH': u.Quantity( 8.420e-8, 'W/(m2 um)'),
-      'CN': u.Quantity( 8.6e-8,   'W/(m2 um)'),
-      'C3': u.Quantity( 8.160e-8, 'W/(m2 um)'),
-     'CO+': u.Quantity( 7.323e-8, 'W/(m2 um)'),
-      'C2': u.Quantity( 3.887e-8, 'W/(m2 um)'),
-    'H2O+': u.Quantity( 1.380e-8, 'W/(m2 um)'),
-      'UC': u.Quantity( 7.802e-8, 'W/(m2 um)'),
-      'BC': u.Quantity( 6.210e-8, 'W/(m2 um)'),
-      'GC': u.Quantity( 3.616e-8, 'W/(m2 um)'),
-      'RC': u.Quantity( 1.316e-8, 'W/(m2 um)'),
-       'R': u.Quantity( 2.177e-8, 'W/(m2 um)'), # Bessell 1998
-       'V': u.Quantity( 3.631e-8, 'W/(m2 um)'), # Bessell 1998
-  'SDSS-R': u.Quantity( 2.812e-8, 'W/(m2 um)'), # Smith et al. 2002 for zeropoint in Jy and effective wavelength 6222 Å.
+    'OH': u.Quantity(10.560e-8, 'W/(m2 um)'),
+      'NH': u.Quantity(8.420e-8, 'W/(m2 um)'),
+      'CN': u.Quantity(8.6e-8,   'W/(m2 um)'),
+      'C3': u.Quantity(8.160e-8, 'W/(m2 um)'),
+     'CO+': u.Quantity(7.323e-8, 'W/(m2 um)'),
+      'C2': u.Quantity(3.887e-8, 'W/(m2 um)'),
+    'H2O+': u.Quantity(1.380e-8, 'W/(m2 um)'),
+      'UC': u.Quantity(7.802e-8, 'W/(m2 um)'),
+      'BC': u.Quantity(6.210e-8, 'W/(m2 um)'),
+      'GC': u.Quantity(3.616e-8, 'W/(m2 um)'),
+      'RC': u.Quantity(1.316e-8, 'W/(m2 um)'),
+       'R': u.Quantity(2.177e-8, 'W/(m2 um)'),  # Bessell 1998
+       'V': u.Quantity(3.631e-8, 'W/(m2 um)'),  # Bessell 1998
+  'SDSS-R': u.Quantity(2.812e-8, 'W/(m2 um)'),  # Smith et al. 2002 for zeropoint in Jy and effective wavelength 6222 Å.
    'SDSSr': u.Quantity(2.812e-8, 'W/(m2 um)'),
 }
 
 MmBC_sun = {  # M - BC for the Sun
-      'OH':  1.791,
+    'OH':  1.791,
       'NH':  1.188,
       'CN':  1.031,
       'C3':  0.497,
@@ -122,7 +122,7 @@ MmBC_sun = {  # M - BC for the Sun
 }
 
 gamma_XX_XX = {
-      'OH': u.Quantity(1.698e-2, '1/AA'),
+    'OH': u.Quantity(1.698e-2, '1/AA'),
       'NH': u.Quantity(1.907e-2, '1/AA'),
       'CN': u.Quantity(1.812e-2, '1/AA'),
       'C3': u.Quantity(3.352e-2, '1/AA'),
@@ -132,7 +132,7 @@ gamma_XX_XX = {
 }
 
 gamma_prime_XX_XX = {
-      'OH': 0.98,
+    'OH': 0.98,
       'NH': 0.99,
       'CN': 0.99,
       'C3': 0.19,
@@ -147,7 +147,7 @@ gamma_prime_XX_XX = {
 # For Msun at r', use Rsun, (V-R)sun = 0.370, and transformation from
 # Smith et al. 2002: r' = V - 0.81 (V - R) + 0.13.  This is close to
 # the -26.95 used by Ivezić et al. 2001.
-Msun = {  
+Msun = {
     'OH': -24.443,
     'NH': -25.046,
     'CN': -25.203,
@@ -161,27 +161,28 @@ Msun = {
     'RC': -27.510,
      'V': -26.76,  # Bessell 1998
      'R': -27.13,  # Bessell 1998, (V-R)sun=0.370 (Colina et al. 1996)
-'SDSS-R': -26.93,  # R to r' via Smith et al. 2002 + Bessel 1998
+    'SDSS-R': -26.93,  # R to r' via Smith et al. 2002 + Bessel 1998
  'SDSSr': -26.93,
 }
 
 S0 = {  # Solar flux density at 1 AU, F_0 * 10**(-0.4 * Msun)
-    'OH': u.Quantity( 632.2, 'W/(m2 um)'),
-    'NH': u.Quantity( 878.4, 'W/(m2 um)'),
+    'OH': u.Quantity(632.2, 'W/(m2 um)'),
+    'NH': u.Quantity(878.4, 'W/(m2 um)'),
     'CN': u.Quantity(1036.8, 'W/(m2 um)'),
     'C3': u.Quantity(1608.8, 'W/(m2 um)'),
    'CO+': u.Quantity(1671.4, 'W/(m2 um)'),
     'C2': u.Quantity(1788.2, 'W/(m2 um)'),
   'H2O+': u.Quantity(1358.6, 'W/(m2 um)'),
-    'UC': u.Quantity( 881.9, 'W/(m2 um)'),
+    'UC': u.Quantity(881.9, 'W/(m2 um)'),
     'BC': u.Quantity(1935.0, 'W/(m2 um)'),
     'GC': u.Quantity(1797.3, 'W/(m2 um)'),
     'RC': u.Quantity(1328.2, 'W/(m2 um)'),
      'V': u.Quantity(1836.4, 'W/(m2 um)'),
      'R': u.Quantity(1548.3, 'W/(m2 um)'),
-'SDSS-R': u.Quantity(1663.5, 'W/(m2 um)'),
+    'SDSS-R': u.Quantity(1663.5, 'W/(m2 um)'),
  'SDSSr': u.Quantity(1663.5, 'W/(m2 um)')
 }
+
 
 def cal_oh(oh, oh_unc, OH, z_true, b, c, E_bc, h, guess=(20, 0.15),
            covar=False):
@@ -208,7 +209,7 @@ def cal_oh(oh, oh_unc, OH, z_true, b, c, E_bc, h, guess=(20, 0.15),
     h : Quantity
       The observer's elevation.
     guess : array, optional
-      An intial guess for the fitting algorithm: OH zero point, BC zero point, 
+      An intial guess for the fitting algorithm: OH zero point, BC zero point,
     covar : bool, optional
       Set to `True` to return the covariance matrix.
 
@@ -247,6 +248,7 @@ def cal_oh(oh, oh_unc, OH, z_true, b, c, E_bc, h, guess=(20, 0.15),
     else:
         return fit, err
 
+
 def continuum_color(w0, m0, m0_unc, w1, m1, m1_unc):
     """Comet continuum color.
 
@@ -257,7 +259,7 @@ def continuum_color(w0, m0, m0_unc, w1, m1, m1_unc):
 
     Distinguishing between the central filter and the other filter is
     not important, except for computing the uncertainty.
-    
+
     Parameters
     ----------
     w0 : string
@@ -292,6 +294,7 @@ def continuum_color(w0, m0, m0_unc, w1, m1, m1_unc):
 
     return Rm, Rm_unc
 
+
 def continuum_colors(m, unc=None):
     """Convert observed HB magntiudes into continnum colors.
 
@@ -315,12 +318,13 @@ def continuum_colors(m, unc=None):
     """
 
     from collections import OrderedDict
-    
+
     if unc is None:
         unc = dict.fromkeys(m, 0)
         return continuum_colors(m, unc=unc)[0]
-    
-    continuum_filters = m.keys()#[f for f in m if f in ['UC', 'BC', 'GC', 'RC']]
+
+    continuum_filters = m.keys()
+                               #[f for f in m if f in ['UC', 'BC', 'GC', 'RC']]
     continuum_filters.sort(key=cw.get)
     Rm = OrderedDict()
     Rm_unc = OrderedDict()
@@ -333,6 +337,7 @@ def continuum_colors(m, unc=None):
         Rm_unc['-'.join((left, right))] = c[1]
 
     return Rm, Rm_unc
+
 
 def estimate_continuum(base_filter, m, unc=None, Rm=None, Rm_unc=None):
     """Continuum flux density, based on the measured continuum.
@@ -374,7 +379,7 @@ def estimate_continuum(base_filter, m, unc=None, Rm=None, Rm_unc=None):
       The uncertianties on `fluxd`, if `unc` was provided.
 
     """
-    
+
     from collections import OrderedDict
     from operator import itemgetter
 
@@ -384,8 +389,8 @@ def estimate_continuum(base_filter, m, unc=None, Rm=None, Rm_unc=None):
 
     if Rm is None:
         if len(m) == 1:
-            Rm = 0 * u.mag / u.Unit('0.1 um')
-            Rm_unc = 0 * u.mag / u.Unit('0.1 um')
+            Rm = dict.fromkeys(m, 0 * u.mag / u.Unit('0.1 um'))
+            Rm_unc = dict.fromkeys(m, 0 * u.mag / u.Unit('0.1 um'))
         else:
             Rm = {}
             Rm_unc = {}
@@ -409,7 +414,7 @@ def estimate_continuum(base_filter, m, unc=None, Rm=None, Rm_unc=None):
         else:
             assert Rm_unc.unit.is_equivalent(u.mag / (0.1 * u.um))
             Rm_unc = dict.fromkeys(m, Rm_unc)
-        
+
     # Farnham et al. recommends UC-BC for all filters, except BC-GC
     # for C2 and GC-RC for H2O+.  Here, we are always going to use the
     # basis filter.  Select the other filter based on Δλ to each
@@ -439,9 +444,9 @@ def estimate_continuum(base_filter, m, unc=None, Rm=None, Rm_unc=None):
         fluxd_unc[f] = np.sqrt(mxx_unc2) * fluxd[f] / 1.0857
 
     return fluxd, fluxd_unc
-  
-def ext_aerosol_bc(E_bc, h):
 
+
+def ext_aerosol_bc(E_bc, h):
     """Aerosol extinction for BC filter.
 
     E_A_BC, Eq. 13 of Farnham et al. 2000.
@@ -462,6 +467,7 @@ def ext_aerosol_bc(E_bc, h):
 
     return E_bc - 0.2532 * np.exp(-h.to(u.km).value / 7.5)
 
+
 def ext_aerosol_oh(E_bc, h):
     """Aerosol extinction for OH filter.
 
@@ -481,8 +487,9 @@ def ext_aerosol_oh(E_bc, h):
 
     """
 
-    #return (3097 / 4453.)**-0.8 * E_aerosol_bc(E_bc, h)
+    # return (3097 / 4453.)**-0.8 * E_aerosol_bc(E_bc, h)
     return 1.33712 * ext_aerosol_bc(E_bc, h)
+
 
 def ext_ozone_oh(z_true, toz, c):
     """Ozone extinction for OH filter.
@@ -511,28 +518,28 @@ def ext_ozone_oh(z_true, toz, c):
     if isinstance(c, str):
         assert c.upper() in ['B', 'G', 'OH', '25%'], 'Invalid c name'
         if c.upper() == 'B':
-            c = np.array([[ 1.323e-2, -1.605e-1,  4.258e-1,  9.099e-1],
+            c = np.array([[1.323e-2, -1.605e-1,  4.258e-1,  9.099e-1],
                           [-1.731e-2,  3.273,    -2.815e-1, -2.221],
-                          [ 5.349e-3, -5.031e-2, -4.182e-1,  1.649],
-                          [ 2.810e-4, -1.195e-2,  1.063e-1, -3.877e-1]])
+                          [5.349e-3, -5.031e-2, -4.182e-1,  1.649],
+                          [2.810e-4, -1.195e-2,  1.063e-1, -3.877e-1]])
         elif c.upper() == 'G':
-            c = np.array([[ 2.880e-2, -3.912e-1,  1.597,    -7.460e-1],
+            c = np.array([[2.880e-2, -3.912e-1,  1.597,    -7.460e-1],
                           [-5.284e-2,  3.753,    -2.632,     8.912e-1],
-                          [ 2.634e-2, -3.380e-1,  8.699e-1,  4.253e-2],
+                          [2.634e-2, -3.380e-1,  8.699e-1,  4.253e-2],
                           [-3.141e-3,  3.359e-2, -9.235e-2, -1.677e-1]])
         elif c.upper() == 'OH':
             c = np.zeros((4, 4))
             c[1, 0] = -1.669e-3
-            c[1, 1] =  3.365
+            c[1, 1] = 3.365
             c[1, 2] = -2.973e-2
-            c[2, 0] =  3.521e-4
+            c[2, 0] = 3.521e-4
             c[2, 1] = -6.662e-3
             c[2, 2] = -5.335e-2
         elif c.upper() == '25%':
             c = np.array([[-2.523e-2,  4.382e-1, -2.415,     4.993],
-                          [ 4.276e-2,  2.538,     4.109,    -8.518],
+                          [4.276e-2,  2.538,     4.109,    -8.518],
                           [-2.510e-2,  4.148e-1, -2.405,     4.666],
-                          [ 5.070e-3, -8.168e-2,  4.272e-1, -8.618e-1]])
+                          [5.070e-3, -8.168e-2,  4.272e-1, -8.618e-1]])
     else:
         c = np.array(c)
         assert c.shape == (4, 4), "c should have shape (4, 4)"
@@ -547,6 +554,7 @@ def ext_ozone_oh(z_true, toz, c):
         G = np.r_[[np.dot(a, np.r_[1, x, x**2, x**3]) for x in X]]
 
     return G
+
 
 def ext_rayleigh_oh(z_true, h, b):
     """Rayliegh extinction for OH filter.
@@ -595,6 +603,7 @@ def ext_rayleigh_oh(z_true, h, b):
 
     return G
 
+
 def ext_total_oh(toz, z_true, b, c, E_bc, h):
     """Total OH extinction.
 
@@ -615,7 +624,7 @@ def ext_total_oh(toz, z_true, b, c, E_bc, h):
     h : Quantity
       The observer's elevation.
     guess : array, optional
-      An intial guess for the fitting algorithm: OH zero point, BC zero point, 
+      An intial guess for the fitting algorithm: OH zero point, BC zero point,
     covar : bool, optional
       Set to `True` to return the covariance matrix.
 
@@ -624,6 +633,7 @@ def ext_total_oh(toz, z_true, b, c, E_bc, h):
     return (ext_rayleigh_oh(z_true, h, b)
             + ext_aerosol_oh(E_bc, h) * airmass_app(z_true, h)
             + ext_ozone_oh(z_true, toz, c))
+
 
 def flux_oh(oh, oh_unc, m, unc, zp, toz, z_true, E_bc, h, Rm=None, Rm_unc=None):
     """Flux from OH.
@@ -691,15 +701,15 @@ def flux_oh(oh, oh_unc, m, unc, zp, toz, z_true, E_bc, h, Rm=None, Rm_unc=None):
 
     frac = fc['OH'] / f  # fraction that is continuum
     frac_unc = np.sqrt(f_unc**2 * fc['OH']**2 + fc_unc['OH']**2 * f**2) / f**2
-    #E_unc = frac_unc / frac * 1.0857
-    #E_unc = np.sqrt(oh_unc**2 + ((fc_unc / fc) * 1.0857)**2)
+    # E_unc = frac_unc / frac * 1.0857
+    # E_unc = np.sqrt(oh_unc**2 + ((fc_unc / fc) * 1.0857)**2)
 
     if frac <= 0.25:
         E_tot = 4 * ((0.25 - frac) * E_0 + frac * E_25)
         E_unc = 4 * (E_0 + E_25) * frac_unc
     else:
-        #assert frac < 0.25, "Continuum = {:%}, more than 25% of observed OH band flux density.  Untested code.".format(frac)
-        #print("Etot", 4 * ((0.25 - frac) * E_0 + frac * E_25), E_tot)
+        # assert frac < 0.25, "Continuum = {:%}, more than 25% of observed OH band flux density.  Untested code.".format(frac)
+        # print("Etot", 4 * ((0.25 - frac) * E_0 + frac * E_25), E_tot)
         # the following yields the same as above at the <0.001 mag level
         E_tot = ((1 - frac) * E_25 + (frac - 0.25) * E_100) / 0.75
         E_unc = np.abs(E_100 - E_25) * frac_unc / 0.75
@@ -712,6 +722,7 @@ def flux_oh(oh, oh_unc, m, unc, zp, toz, z_true, E_bc, h, Rm=None, Rm_unc=None):
 
     f_oh, f_oh_unc = flux_gas(f, f_unc, fc['OH'], fc_unc['OH'], 'OH')
     return E_tot, E_unc, m, m_unc, f_oh, f_oh_unc
+
 
 def flux_gas(f, f_unc, fc, fc_unc, filt):
     """Gas emission band total flux.
@@ -754,6 +765,7 @@ def flux_gas(f, f_unc, fc, fc_unc, filt):
     fgas_unc = np.sqrt(f_unc**2 + fc_unc**2) / gamma_XX_XX[filt]
 
     return fgas.decompose([u.W, u.m]), fgas_unc.decompose([u.W, u.m])
+
 
 def Rm2S(w0, w1, Rm, Rm_unc=None):
     """Convert continuum color, `Rm`, to spectral slope, `S`.
@@ -814,6 +826,7 @@ def Rm2S(w0, w1, Rm, Rm_unc=None):
 
     return S
 
+
 def S2Rm(w0, w1, S, S_unc=None):
     """Convert spectral slope `S` to continuum color, `Rm`.
 
@@ -868,7 +881,7 @@ def S2Rm(w0, w1, S, S_unc=None):
     dw = w1 - w0
     beta = 2 / (S * 100 * u.percent) / dw
     Rm = 2.5 / dw * np.log10((beta + 1) / (beta - 1)) * u.mag
-    
+
     if S_unc is not None:
         Rm_unc = Rm * S_unc / S
         return Rm, Rm_unc
