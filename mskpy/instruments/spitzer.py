001326cbfff919ef3aca03a9ea348f52904fb268
Michael S. P. Kelley <msk@astro.umd.edu>
2020-02-09 10:07:30 -0500

    Capitalize IRS module

--- mskpy/instruments/spitzer.py
+++ mskpy/instruments/spitzer.py
@@ -1031,7 +1031,7 @@ class IRSCombine:
             'comment': 'Slit position angle (E of N)'
         }
         for module, pa in position_angles.items():
-            self.header['position angle'][module] = (
+            self.header['position angle'][module.upper()] = (
                 '{:.2f}'.format(float(np.mean(pa)) * u.deg))
 
         c = SkyCoord(self.header['RA'][0],
