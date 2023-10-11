4120833c08245808dbff72bc88060ed12c4b3859
Michael S. P. Kelley <msk@astro.umd.edu>
2023-10-10 20:08:55 -0400

    Support for PSG models and running configurations through the API.

--- mskpy/models/psg/__init__.py
+++ mskpy/models/psg/__init__.py
@@ -7,8 +7,13 @@ Support for the Planetary Spectrum Generator
 
 import os
 import re
-from collections import UserDict
+from collections import UserDict, defaultdict
+import requests
 import numpy as np
+from astropy.table import Table
+
+__all__ = ["PSGConfig", "PSGModel"]
+
 
 class PSGConfig(UserDict):
     @classmethod
@@ -56,7 +61,7 @@ class PSGConfig(UserDict):
         for value in values.split(","):
             value_list.append(cls._parse_value(value))
         return value_list
-    
+
     def __str__(self):
         s = ""
         for key, value in self.items():
@@ -65,7 +70,7 @@ class PSGConfig(UserDict):
 
     def write(self, filename, overwrite=False):
         """Write to file.
-        
+
 
         Parameters
         ----------
@@ -74,12 +79,13 @@ class PSGConfig(UserDict):
 
         overwrite : bool, optional
             If the file exists, overwrite it.
-        
+
         """
 
         if os.path.exists(filename) and not overwrite:
-            raise RuntimeError("File exists, overwrite it with overwrite=True.")
-        
+            raise RuntimeError(
+                "File exists, overwrite it with overwrite=True.")
+
         with open(filename, "w") as outf:
             for key, value in self.items():
                 outf.write(f"<{key}>{self._format_value(value)}\n")
@@ -93,3 +99,77 @@ class PSGConfig(UserDict):
         else:
             formatted_value = str(value)
         return formatted_value
+
+    def run(self, fn):
+        """Run the PSG with this configuration.
+
+
+        Parameters
+        ----------
+
+        fn : string
+            Save results to this file name.  An existing file will be
+            overwritten.
+
+        """
+
+        data = {"file": str(self)}
+        response = requests.post(
+            "https://psg.gsfc.nasa.gov/api.php", data=data)
+        response.raise_for_status()
+        with open(fn, "w") as outf:
+            outf.write(response.text)
+
+
+class PSGModel:
+    """PSG model spectra."""
+
+    def __init__(self, fn):
+        with open(fn, "r") as model:
+            reading_data = False
+            rows = []
+            for line in model:
+                line = line.strip()
+                if len(line) == 0:
+                    continue
+                elif line.startswith("# Molecules considered:"):
+                    molecules = [x.strip()
+                                 for x in line.split(":")[1].split(",")]
+                    continue
+                elif "Molecular sources" in line:
+                    sources = [x.strip()
+                               for x in line.split(":")[1].split(",")]
+                    if len(set(sources)) != len(sources):
+                        raise ValueError(
+                            "Molecular source names are not unique:" + ",".join(sources))
+
+                    self.molecules = defaultdict(list)
+                    for m, source in zip(molecules, sources):
+                        self.molecules[m].append(source)
+
+                    continue
+                elif line.startswith("# Wave/freq"):
+                    reading_data = True
+                    self.source_molecules = defaultdict(list)
+                    columns = line[2:].split()
+                    n = len(sources)
+                    columns = ["wave"] + columns[1:-n] + sources
+                    continue
+
+                if reading_data:
+                    data = [float(x) for x in line.split()]
+                    row = {k: v for k, v in zip(columns, data)}
+                    rows.append(row)
+
+        self.data = Table(rows)
+
+    def __getitem__(self, k):
+        if k in self.molecules:
+            spec = 0
+            i = 0
+            for source in self.molecules[k]:
+                spec = spec + self.data[source].data
+
+            return spec
+        else:
+            return self.data[k]
--- mskpy/models/psg/test/test_psg.py
+++ mskpy/models/psg/test/test_psg.py
@@ -1,25 +1,29 @@
 from astropy.utils.data import get_pkg_data_filename
 from mskpy.models import psg
 
-def test_parse():
+
+def test_parse_config():
     filename = get_pkg_data_filename("psg_cfg.txt", "mskpy.models.psg.test")
     config = psg.PSGConfig.from_file(filename)
 
     assert config["OBJECT"] == "Comet"
     assert config["OBJECT-NAME"] == "22P"
     assert config["OBJECT-OBS-VELOCITY"] == -5.887
-    assert config["ATMOSPHERE-TYPE"] == ["GSFC[CH4]", "GSFC[C2H6]", "GSFC[CH3OH]", "GSFC[CH3OH_V9]"]
+    assert config["ATMOSPHERE-TYPE"] == ["GSFC[CH4]",
+                                         "GSFC[C2H6]", "GSFC[CH3OH]", "GSFC[CH3OH_V9]"]
     assert config["ATMOSPHERE-WEIGHT"] == 541.40
     assert config["ATMOSPHERE-NGAS"] == 4
     assert config["ATMOSPHERE-PRESSURE"] == 1e27
     assert config["ATMOSPHERE-AEROS"] == None
 
-def test_str():
+
+def test_config_str():
     config = psg.PSGConfig()
     config["OBJECT"] = "Comet"
     config["OBJECT-NAME"] = "22P"
     config["OBJECT-OBS-VELOCITY"] = -5.887
-    config["ATMOSPHERE-TYPE"] = ["GSFC[CH4]", "GSFC[C2H6]", "GSFC[CH3OH]", "GSFC[CH3OH_V9]"]
+    config["ATMOSPHERE-TYPE"] = ["GSFC[CH4]",
+                                 "GSFC[C2H6]", "GSFC[CH3OH]", "GSFC[CH3OH_V9]"]
     config["ATMOSPHERE-WEIGHT"] = 541.40
     config["ATMOSPHERE-NGAS"] = 4
     config["ATMOSPHERE-PRESSURE"] = 1e27
@@ -35,3 +39,19 @@ def test_str():
 <ATMOSPHERE-PRESSURE>1e+27
 <ATMOSPHERE-AEROS>
 """
+
+
+def test_parse_model():
+    filename = get_pkg_data_filename("psg_rad.txt", "mskpy.models.psg.test")
+    model = psg.PSGModel(filename)
+    columns = ["wave", "Total", "22P", "Nucleus", "Dust",
+               "GSFC[CH4]", "GSFC[C2H6]", "GSFC[CH3OH]", "GSFC[CH3OH_V9]"]
+    values = [3.267386380, 1.4972492e-05, 3.34337e-02, 2.60666e-02,
+              7.35209e-03, 1.48372e-06, 2.71464e-07, 1.34468e-06, 1.19062e-05]
+    for k, v in zip(columns, values):
+        assert model[k][521] == v
+        assert model[521][k] == v
+
+    assert model["CH4"][521] == values[5]
+    assert model["C2H6"][521] == values[6]
+    assert model["CH3OH"][521] == values[7] + values[8]
